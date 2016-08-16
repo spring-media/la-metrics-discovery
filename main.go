69d82e09a082311d870343f8fee83c0dbe0c74c8
Stoyan Stoyanov <stoyan.stoyanov@weltn24.de>
2016-08-16 14:31:57 +0200

    fix the append issue

--- main.go
+++ main.go
@@ -25,7 +25,9 @@ func main() {
 		list          interface{}
 		err           error
 	)
+
 	flag.Parse()
+
 	awsSession := session.New(aws.NewConfig().WithRegion(*awsRegion))
 
 	switch *discoveryType {
@@ -47,7 +49,9 @@ func main() {
 	default:
 		log.Fatalf("discovery type %s not supported", *discoveryType)
 	}
+
 	err = json.NewEncoder(os.Stdout).Encode(Result{Data: list})
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -57,15 +61,18 @@ func getAllDBInstances(rdsCli interface {
 	DescribeDBInstances(input *rds.DescribeDBInstancesInput) (*rds.DescribeDBInstancesOutput, error)
 }) ([]map[string]string, error) {
 	resp, err := rdsCli.DescribeDBInstances(&rds.DescribeDBInstancesInput{})
+
 	if err != nil {
 		return nil, fmt.Errorf("getting RDS instances:%v", err)
 	}
 
 	rdsIdentifiers := make([]map[string]string, len(resp.DBInstances))
-	for _, rds := range resp.DBInstances {
-		rdsIdentifiers = append(rdsIdentifiers, map[string]string{
+
+	for ctr, rds := range resp.DBInstances {
+
+		rdsIdentifiers[ctr] = map[string]string{
 			"{#RDSIDENTIFIER}": *rds.DBInstanceIdentifier,
-		})
+		}
 	}
 	return rdsIdentifiers, nil
 }
@@ -73,33 +80,41 @@ func getAllDBInstances(rdsCli interface {
 func getAllCloudFrontDistributions(cloudFrontCli interface {
 	ListDistributions(*cloudfront.ListDistributionsInput) (*cloudfront.ListDistributionsOutput, error)
 }) ([]map[string]string, error) {
+
 	resp, err := cloudFrontCli.ListDistributions(&cloudfront.ListDistributionsInput{})
+
 	if err != nil {
 		return nil, fmt.Errorf("listing CloudFront distributions %v", err)
 	}
 
 	dists := make([]map[string]string, len(resp.DistributionList.Items))
-	for _, dist := range resp.DistributionList.Items {
-		dists = append(dists, map[string]string{
+
+	for ctr, dist := range resp.DistributionList.Items {
+		dists[ctr] = map[string]string{
 			"{#DISTID}": *dist.Id,
-		})
+		}
 	}
+
 	return dists, nil
 }
 
 func getAllElasticLoadBalancers(elbCli interface {
 	DescribeLoadBalancers(*elb.DescribeLoadBalancersInput) (*elb.DescribeLoadBalancersOutput, error)
 }) ([]map[string]string, error) {
+
 	resp, err := elbCli.DescribeLoadBalancers(&elb.DescribeLoadBalancersInput{})
+
 	if err != nil {
 		return nil, fmt.Errorf("reading ELBs:%v", err)
 	}
 
 	elbs := make([]map[string]string, len(resp.LoadBalancerDescriptions))
-	for _, elb := range resp.LoadBalancerDescriptions {
-		elbs = append(elbs, map[string]string{
+
+	for ctr, elb := range resp.LoadBalancerDescriptions {
+		elbs[ctr] = map[string]string{
 			"{#LOADBALANCERNAME}": *elb.LoadBalancerName,
-		})
+		}
 	}
+
 	return elbs, nil
 }
